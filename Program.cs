58d69f8bea36fcd8acf159f566831a8f27918d91
Howell <469434205@qq.com>
2025-03-21 16:33:00 +0800

    1111

--- Program.cs
+++ Program.cs
@@ -1,8 +1,2 @@
-using System;
-namespace HelloWorld{
-    class Program{
-        static void Main(string[] args){
-            Console.WriteLine("Hello,World!");
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
